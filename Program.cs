fd8dc6e273c9c6bac6ecf0c140fcb9a8f73c9a61
Greg Lincoln <greg@greglincoln.com>
2017-02-10 14:41:13 -0500

    initial checkin.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NodaTime;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IClock clock = SystemClock.Instance; // red squiggly for missing mscorlib. 
+            Console.WriteLine("Hello world {0}.", clock.Now.ToString());
+        }
+    }
+}
